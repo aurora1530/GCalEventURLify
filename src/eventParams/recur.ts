828a3a4115742dbb6e97e4de1af443d2e67c1159
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-04-22 09:22:44 +0900

    add: 繰り返しパラメータを追加

---
+++ src/eventParams/recur.ts
@@ -0,0 +1,41 @@
+import { EventParam } from './eventParam';
+import { dateToRFC5545, getObjectKeys } from '../utils';
+
+type RecurConf = {
+  freq?: 'SECONDLY' | 'MINUTELY' | 'HOURLY' | 'DAILY' | 'WEEKLY' | 'MONTHLY' | 'YEARLY';
+  interval: number;
+  until?: Date;
+  count?: number;
+};
+
+export class RecurParam extends EventParam {
+  public readonly key = 'recur';
+  public readonly value: string;
+
+  constructor(
+    recurConf: RecurConf = {
+      interval: 1,
+    }
+  ) {
+    super();
+    this.value = RecurParam._confToValue(recurConf);
+  }
+
+  private static _confToValue(conf: RecurConf): string {
+    return getObjectKeys(conf)
+      .flatMap((key) => {
+        const keyStr = key.toUpperCase();
+        const value = conf[key];
+        if (value === undefined) return [];
+
+        const valueStr = value instanceof Date ? dateToRFC5545(value) : value.toString();
+
+        return `${keyStr}=${valueStr}`;
+      })
+      .join(';');
+  }
+
+  public encoded(): string {
+    return `${this.key}=RRULE:${encodeURIComponent(this.value)}`;
+  }
+}
