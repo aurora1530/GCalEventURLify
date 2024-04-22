735574e368d962ee51b3b766a5795b1a706db7b1
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-04-22 09:15:49 +0900

    add: オブジェクトのkeyを型安全に取得する関数を追加

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export const dateToRFC5545 = (date: Date): string => {
   return date.toISOString().replace(/-|:|\.\d+/g, '');
 };
+
+export const getObjectKeys = <T extends object>(obj: T): Array<keyof T> => {
+  return Object.keys(obj) as Array<keyof T>;
+};
